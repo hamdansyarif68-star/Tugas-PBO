1275faf6874e034768c55c9516e1c4100450f2d1
hamdansyarif68-star <hamdansyarif68@gmail.com>
2026-04-05 00:17:01 +0800

    Update Incr.java

--- Tugas1/ Tugas1_SourceCode_Praktek/Incr.java
+++ Tugas1/ Tugas1_SourceCode_Praktek/Incr.java
@@ -1,1 +1,10 @@
-
+//Nama                  : Muh. Hamdan Syarif
+//NIM                   : 13020240240
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 08.00public class Incr {
+    public static void main(String[] args) {
+        int i, j;
+        i = 3;
+        j = i++;
+        System.out.println("Nilai i : " + (+i) + "\nNilai j : " + j);
+    }
+}
