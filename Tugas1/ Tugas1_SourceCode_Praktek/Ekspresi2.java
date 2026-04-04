2c36c0d5eb45306bf0510dcb3156a47b7fee02b6
hamdansyarif68-star <hamdansyarif68@gmail.com>
2026-04-05 00:16:40 +0800

    Update Ekspresi2.java

--- Tugas1/ Tugas1_SourceCode_Praktek/Ekspresi2.java
+++ Tugas1/ Tugas1_SourceCode_Praktek/Ekspresi2.java
@@ -1,1 +1,26 @@
-
+//Nama                  : Muh. Hamdan Syarif
+//NIM                   : 13020240240
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 08.00public class Ekspresi2 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        float fx;
+        float fy;
+        
+        System.out.print("x/y (format integer) = " + x / y);
+        System.out.print("\nx/y (format float) = " + x / y);
+        
+        fx = x;
+        fy = y;
+        System.out.print("\nx/y (format integer) = " + fx / fy);
+        System.out.print("\nx/y (format float) = " + fx / fy);
+        
+        System.out.print("\nfloat(x)/float(y) (format integer)= " + (float) x / (float) y);
+        System.out.print("\nfloat(x)/float(y) (format float) = " + (float) x / (float) y);
+        
+        x = 10;
+        y = 3;
+        System.out.print("\nx/y (format integer) = " + x / y);
+        System.out.print("\nx/y (format float) = " + x / y);
+    }
+}
