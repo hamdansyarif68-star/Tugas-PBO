72609f18abfcd114d7fb76a5bcc056b3b98b25cd
hamdansyarif68-star <hamdansyarif68@gmail.com>
2026-04-05 00:22:37 +0800

    Update asign2.java

--- Tugas1/ Tugas1_SourceCode_Praktek/asign2.java
+++ Tugas1/ Tugas1_SourceCode_Praktek/asign2.java
@@ -1,1 +1,10 @@
-
+//Nama                  : Muh. Hamdan Syarif
+//NIM                   : 13020240240
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 08.00
+public class Asign {
+    public static void main(String[] args) {
+        int i;
+        System.out.print("hello\n");
+        i = 5;
+        System.out.println("Ini nilai i : " + i);}
+}
