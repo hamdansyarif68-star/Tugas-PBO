017e8b454df1a57fc8b884e40d1c62ce57d04800
hamdansyarif68-star <hamdansyarif68@gmail.com>
2026-04-05 00:17:31 +0800

    Update Oper1.java

--- Tugas1/ Tugas1_SourceCode_Praktek/Oper1.java
+++ Tugas1/ Tugas1_SourceCode_Praktek/Oper1.java
@@ -1,1 +1,17 @@
-
+//Nama                  : Muh. Hamdan Syarif
+//NIM                   : 13020240240
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 08.00public class Oper1 {
+    public static void main(String[] args) {
+        int n = 10;
+        int x = 1;
+        int y = 2;
+        
+        System.out.println("n = " + n);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("n & 8 = " + (n & 8));
+        System.out.println("x & ~ 8 = " + (x & ~8));
+        System.out.println("y << 2 = " + (y << 2));
+        System.out.println("y >> 3 = " + (y >> 3));
+    }
+}
