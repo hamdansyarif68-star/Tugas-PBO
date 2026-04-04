20d165886d7ef29239271cd63624f7d50ad601d6
hamdansyarif68-star <hamdansyarif68@gmail.com>
2026-04-05 00:16:03 +0800

    Update Ekspresi.java

--- Tugas1/ Tugas1_SourceCode_Praktek/Ekspresi.java
+++ Tugas1/ Tugas1_SourceCode_Praktek/Ekspresi.java
@@ -1,1 +1,12 @@
-
+//Nama                  : Muh. Hamdan Syarif
+//NIM                   : 13020240240
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 08.00public class Ekspresi {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        
+        System.out.print("x = " + x + "\n");
+        System.out.print("y = " + y + "\n");
+        System.out.print("hasil ekspresi = (x<y)?x:y = " + ((x < y) ? x : y));
+    }
+}
