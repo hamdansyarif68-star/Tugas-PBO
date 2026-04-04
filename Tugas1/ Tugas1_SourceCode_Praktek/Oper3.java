19c155fa1651369158749847b31fe3d3010b56a6
hamdansyarif68-star <hamdansyarif68@gmail.com>
2026-04-05 00:19:06 +0800

    Update Oper3.java

--- Tugas1/ Tugas1_SourceCode_Praktek/Oper3.java
+++ Tugas1/ Tugas1_SourceCode_Praktek/Oper3.java
@@ -1,1 +1,22 @@
-
+//Nama                  : Muh. Hamdan Syarif
+//NIM                   : 13020240240
+//Hari,Tanggal, Waktu   : Jum'at/ 2 April 2026/ 08.00
+public class Oper3 {
+    public static void main(String[] args) {
+        if (true && true) {
+            System.out.println(true && true);
+        }
+        if (true & true) {
+            System.out.println(true & false);
+        }
+        if (true) {
+            System.out.println(true);
+        }
+        if (true || true) {
+            System.out.println(true);
+        }
+        if (true | false) {
+            System.out.println(true | false);
+        }
+    }
+}
