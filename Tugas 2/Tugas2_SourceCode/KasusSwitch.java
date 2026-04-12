64508ebf43a3a3ca01c4b2c5a46beba3af0e9bf5
Muh. Hamdan Syarif <hamdansyarif68@gmail.com>
2026-04-12 23:58:40 +0800

    Create KasusSwitch.java

---
+++ Tugas 2/Tugas2_SourceCode/KasusSwitch.java
@@ -0,0 +1,37 @@
+//Nama      : Muh. Hamdan Syarif
+//NIM       : 13020240240
+//Kelas     : A3
+
+import java.util.Scanner;
+
+public class KasusSwitch {
+    public static void main(String[] args) {
+        char cc;
+        Scanner masukan = new Scanner(System.in);
+        
+        System.out.print("Ketikkan sebuah huruf, akhir dengan RETURN \n");
+        cc = masukan.next().charAt(0);
+        
+        switch (cc) {
+            case 'a':
+                System.out.print("Yang anda ketik adalah a \n");
+                break;
+            case 'u':
+                System.out.print("Yang anda ketik adalah u \n");
+                break;
+            case 'e':
+                System.out.print("Yang anda ketik adalah e \n");
+                break;
+            case 'i':
+                System.out.print("Yang anda ketik adalah i \n");
+                break;
+            case 'o':
+                System.out.print("Yang anda ketik adalah o \n");
+                break;
+            default:
+                System.out.print("Yang anda ketik adalah huruf mati \n");
+        }
+        
+        masukan.close();
+    }
+}
