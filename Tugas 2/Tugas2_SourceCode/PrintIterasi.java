3ebf21d3b7e54251ba951f146ba87c883ecc78f5
Muh. Hamdan Syarif <hamdansyarif68@gmail.com>
2026-04-13 00:01:35 +0800

    Create PrintIterasi.java

---
+++ Tugas 2/Tugas2_SourceCode/PrintIterasi.java
@@ -0,0 +1,29 @@
+//Nama      : Muh. Hamdan Syarif
+//NIM       : 13020240240
+//Kelas     : A3
+
+import java.util.Scanner;
+
+public class PrintIterasi {
+    public static void main(String[] args) {
+        int i;
+        int N;
+        Scanner masukan = new Scanner(System.in);
+        
+        System.out.print("Nilai N >0 = ");
+        N = masukan.nextInt();
+        i = 1;
+        
+        System.out.print("Print i dengan ITERATE : \n");
+        for (;;) {
+            System.out.println(i);
+            if (i == N)
+                break;
+            else {
+                i++;
+            }
+        }
+        
+        masukan.close();
+    }
+}
