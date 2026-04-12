803e128986c39e3be195536e5e13e429db957064
Muh. Hamdan Syarif <hamdansyarif68@gmail.com>
2026-04-12 23:57:58 +0800

    Create KasusBoolean.java

---
+++ Tugas 2/Tugas2_SourceCode/KasusBoolean.java
@@ -0,0 +1,14 @@
+//Nama      : Muh. Hamdan Syarif
+//NIM       : 13020240240
+//Kelas     : A3
+
+public class KasusBoolean {
+    public static void main(String[] args) {
+        boolean bool;
+        bool = true;
+        
+        if (bool) {
+            System.out.print("true\n");
+        }
+    }
+}
