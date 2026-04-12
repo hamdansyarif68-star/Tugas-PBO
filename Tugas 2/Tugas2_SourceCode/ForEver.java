40199b0f7a6d46600cd1c7f5cefdc906790e7b4d
Muh. Hamdan Syarif <hamdansyarif68@gmail.com>
2026-04-12 23:47:32 +0800

    Create ForEver.java

---
+++ Tugas 2/Tugas2_SourceCode/ForEver.java
@@ -0,0 +1,12 @@
+//Nama      : Muh. Hamdan Syarif
+//NIM       : 13020240240
+//Kelas     : A3
+
+public class ForEver {
+    public static void main(String[] args) {
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true) {
+            System.out.print("Print satu baris ....\n");
+        }
+    }
+}
